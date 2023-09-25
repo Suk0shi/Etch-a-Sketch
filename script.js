37c58adbf0eebc67e13eaf3aa5b688b8d6a6b903
Suk0shi <hugo.cordon100@gmail.com>
2023-09-25 13:39:20 +0100

    Add working color change on hover

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const container = document.querySelector('#container');
 
 ii = 0
 while (ii < 16) {
+    const flexContainer = document.createElement('div');
+    flexContainer.classList.add('flexContainer');
+    container.appendChild(flexContainer);
     i = 0;
     while (i<16) {
         const content = document.createElement('div');
         content.classList.add('content');
-        container.appendChild(content);
+        flexContainer.appendChild(content);
         i++;
     }
-    const next = document.createElement('div');
-    next.classList.add('next');
-    container.appendChild(next);
+    //const next = document.createElement('div');
+    //next.classList.add('next');
+    //container.appendChild(next);
     ii++;
 }
 
 
-const content = document.querySelectorAll("div");
-console.log(content);
-content.forEach((contents) => {
-    contents.addEventListener("mouseover", (event) => { //fix content.addEventListener is not a function
-    console.log("working");
-    contents.setAttribute('style', 'background: pink');
-    });
+const content = document.getElementsByClassName("content");
 
-});
-
+for (const contents of content) {
+    contents.addEventListener("mouseover", (event) => { 
+        contents.setAttribute('style', 'background: pink');
+    }
+)};
